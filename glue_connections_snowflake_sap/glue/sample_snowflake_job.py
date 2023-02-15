fc1b03154879b76220eca5aa0d65a884f5ea67be
Anatolii Maslov <masslove2@bk.ru>
2023-02-16 01:50:34 +0300

    initial commit

---
+++ glue_connections_snowflake_sap/glue/sample_snowflake_job.py
@@ -0,0 +1,26 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+query = "SELECT table_schema, table_name FROM information_schema.tables LIMIT 10"
+connection_name = "glueconn-awstips-snowflake"
+connection_type = "custom.jdbc"
+connection_options = { "query": query, "connectionName": connection_name}
+
+dynamic_frame = glueContext.create_dynamic_frame.from_options(
+        connection_type=connection_type,
+        connection_options=connection_options,
+        transformation_ctx="dynamic_frame"
+)
+
+dynamic_frame.show()
