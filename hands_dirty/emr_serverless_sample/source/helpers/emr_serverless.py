27baecfad1683f0f11293e8ef9c9ad756d64bd68
Anatolii Maslov <112352074+natmasslove@users.noreply.github.com>
2023-12-17 18:14:49 +0300

    improved output info about the job run, added spark submit parameters (#9)

--- hands_dirty/emr_serverless_sample/source/helpers/emr_serverless.py
+++ hands_dirty/emr_serverless_sample/source/helpers/emr_serverless.py
@@ -1,5 +1,5 @@
 import time
-
+from datetime import datetime
 
 def start_job_run(
     emr_client,
@@ -9,12 +9,21 @@ def start_job_run(
     run_name,
     execution_role_arn,
     logger,
+    sparkSubmitParameters = None,    
 ):
+    if sparkSubmitParameters is None:
+        sparkSubmitParameters = {
+            "spark.hadoop.hive.metastore.client.factory.class" : "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory",
+        }
+
+    sparkSubmitParameters_str = " --conf " + " --conf ".join(f"{k}={v}" for k, v in sparkSubmitParameters.items())
+    print(f"sparkSubmitParameters_str: {sparkSubmitParameters_str}")
+
     job_driver = {
         "sparkSubmit": {
             "entryPoint": script_path,
             "entryPointArguments": arguments,
-            "sparkSubmitParameters": "--conf spark.hadoop.hive.metastore.client.factory.class=com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory",
+            "sparkSubmitParameters": sparkSubmitParameters_str,
         }
     }
     configuration_overrides = {
@@ -37,15 +46,32 @@ def start_job_run(
 
 
 def watch_job_run(emr_client, application_id, run_id, logger):
-    status = None
+    state_durations = {}  # Dictionary to store durations for each state
+    last_state_change = datetime.now()  # Initialize the time for state change
+    current_state = None
 
     response = ""
     # Waiting for Job Run to Complete (reach final state)
-    while status not in ["SUCCESS", "FAILED"]:
+    while current_state not in ["SUCCESS", "FAILED"]:
         response = emr_client.get_job_run(applicationId=application_id, jobRunId=run_id)
-        status = response["jobRun"]["state"]
-        logger.info(status)
+        new_state = response["jobRun"]["state"]
+
+        if new_state != current_state:
+            current_time = datetime.now()
+            if current_state is not None:
+                duration = (current_time - last_state_change).total_seconds()
+                state_durations[current_state] = duration
+                logger.info(f"State {current_state}: {duration} seconds")
+
+            last_state_change = current_time
+            current_state = new_state
+
+        logger.info(new_state)
         time.sleep(2)
+
+    # Add the duration for the final state
+    duration = (datetime.now() - last_state_change).total_seconds()
+    state_durations[current_state] = duration    
 
     i = 0
     # Waiting until execution stats are available (they don't appear in response immediately)
@@ -57,10 +83,16 @@ def watch_job_run(emr_client, application_id, run_id, logger):
         time.sleep(2)
         i += 1
         if i > 200:
-            break
+            raise Exception("Can't retrieve total source utilization stats")
         logger.info(f"Waiting for totalResourceUtilization to be populated.")
 
     totalResourceUtilization = response["jobRun"]["totalResourceUtilization"]
     totalExecutionDurationSeconds = response["jobRun"]["totalExecutionDurationSeconds"]
 
-    return totalResourceUtilization, totalExecutionDurationSeconds
+    output = {
+        "state_durations" : state_durations,
+        "totalResourceUtilization" : totalResourceUtilization,
+        "totalExecutionDurationSeconds" : totalExecutionDurationSeconds,
+    }
+
+    return output
