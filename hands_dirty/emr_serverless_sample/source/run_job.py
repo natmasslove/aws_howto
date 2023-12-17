4c5bc1095acbf42e17aca57a6614069d258776fe
Anatolii Maslov <112352074+natmasslove@users.noreply.github.com>
2023-12-17 18:14:49 +0300

    improved output info about the job run, added spark submit parameters (#9)

--- hands_dirty/emr_serverless_sample/source/run_job.py
+++ hands_dirty/emr_serverless_sample/source/run_job.py
@@ -6,6 +6,7 @@ from helpers.s3_helper import upload_file_to_s3
 from helpers.emr_serverless import start_job_run, watch_job_run
 import logging
 from time import strftime
+import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -13,6 +14,10 @@ handler = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)  # so we see logged messages in console when debugging
+
+file_handler = logging.FileHandler("run_job.log")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 stack_name = "cf-emrsrvless-demo"
 
@@ -38,6 +43,21 @@ strtime = datetime.now().strftime("%Y%m%d%H%M%S")
 run_name = f"{paths[0]}-{strtime}"
 arguments = [s3_bucket]
 
+logger.info(f"Running script {paths[0]}, using arguments: [{', '.join(arguments)}]")
+
+# this is optional - just to demonstrate how you can change compute capacity allocation for the job run
+# you also can leave it empty sparkSubmitParameters = {}
+sparkSubmitParameters = {
+    "spark.hadoop.hive.metastore.client.factory.class" : "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory",    
+    "spark.executor.instances" : "1",
+    "spark.dynamicAllocation.initialExecutors" : "1",
+    "spark.dynamicAllocation.maxExecutors" : "1",
+    "spark.executor.cores" : "2",
+    "spark.executor.memory" : "8G",
+}
+
+start = time.perf_counter()
+
 # Starting Job Run
 job_run_id, response = start_job_run(
     emr_client,
@@ -45,18 +65,22 @@ job_run_id, response = start_job_run(
     script_path,
     arguments,
     run_name,
-    execution_role_arn,
+    execution_role_arn,    
     logger,
+    sparkSubmitParameters,
 )
+
+logger.info(response)
 
 # Waiting for Job Run to Complete and collect stats
-start = time.perf_counter()
-totalResourceUtilization, totalExecutionDurationSeconds = watch_job_run(
+output = watch_job_run(
     emr_client, application_id, job_run_id, logger
 )
+
 end = time.perf_counter()
 
+formatted_output = json.dumps(output, indent=4)
+
 # Print out Run stats
-print(f"Total Resource Utilization: {totalResourceUtilization}")
-print(f"Total Execution Duration: {totalExecutionDurationSeconds} seconds")
-print(f"Total Execution Time: {end - start:0.4f} seconds")
+print(f"Output:\n{formatted_output}")
+print(f"Total Running Time: {end - start:0.4f} seconds")
